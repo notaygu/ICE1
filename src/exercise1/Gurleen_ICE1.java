e4cd9baeba4e9cacf2732e1b67518b2e599198f3
notaygu <notaygu@sheridancollege.ca>
2023-02-01 19:04:06 -0500

    ICE 1 Gurleen Kaur

---
+++ src/exercise1/Gurleen_ICE1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise1;
+
+/**
+ *
+ * @author notay
+ */
+public class Gurleen_ICE1 {
+    
+}
